c31dd485df49771b224eb5e7acca917a44aed4a7
rivolela <contato@pickoout.com.br>
2017-05-05 15:31:19 +0100

    updated informatica dictionary

--- config/departaments/informatica.js
+++ config/departaments/informatica.js
@@ -30,62 +30,65 @@ module.exports = {
     	},
     	{
     		query : "notebook", 
-    	dictionary: 'merchantcategory=' + 'Informática / Notebooks e Conversíveis / Notebooks' + '&' +
-					'merchantcategory=' + 'Notebooks & Ultrabooks' + '&' +
-					'merchantcategory=' + 'Informatica / Notebooks / Intel Celeron' + '&' +
-					'merchantcategory=' + 'Informatica / Notebooks / Intel Core i3' + '&' +
-					'merchantcategory=' + 'Informatica / Notebooks / Intel Core i5' + '&' +
-					'merchantcategory=' + 'Informatica / Notebooks / Intel Core i7' + '&' +
-					'merchantcategory=' + 'Notebook' + '&' +
-					'merchantcategory=' + 'Nb / - / Core / I7' + '&' +
-					'merchantcategory=' + 'Nb / - / Core / I3' + '&' +
-					'merchantcategory=' + 'Nb / - / Core / I5' + '&' +
-					'merchantcategory=' + 'Nb / - / Core / I7' + '&' +
-					'merchantcategory=' + '2 / Em / 1 / - / Core / I3' + '&' +
-					'merchantcategory=' + '2 / Em / 1 / - / Core / I5' + '&' +
-					'merchantcategory=' + '2 / Em / 1 / - / Core / I7' + '&' +
-					'merchantcategory=' + 'pad / Mini / Wi-fi / + / 3g / & / 4g',
-    	departament:"informática",
-    	programs:'12011,13212,16588,12781,12785,12784,13604,18878,13602'},
-
-    	{query : "macbook apple", 
-    	dictionary: 'merchantcategory=' + 'Informática / Notebooks e Conversíveis / Notebooks' + '&' +
-					'merchantcategory=' + 'Mochilas para Notebook' + '&' +
-					'merchantcategory=' + 'Informática' + '&' +
-					'merchantcategory=' + 'Notebook',
-    	departament:"informática",
-    	programs:'12011,13212,16588,12781,12785,12784,13604,18878,13602'},
-
-    	{query : "impressoras", 
-    	dictionary: 'merchantcategory=' + 'Jato de Tinta' + '&' +
-					'merchantcategory=' + 'Laser Monocromática' + '&' +
-					'merchantcategory=' + 'Laser Colorida' + '&' +
-					'merchantcategory=' + 'Laser P&B' + '&' +
-					'merchantcategory=' + 'Informatica / Impressoras / Multifuncional' + '&' +
-					'merchantcategory=' + 'Informática' + '&' +
-					'merchantcategory=' + 'Informática / Impressão / Impressoras' + '&' +
-					'merchantcategory=' + 'Informatica / Impressoras / Impressora',
-    	departament:"informática",
-    	programs:'12011,13212,16588,12781,12785,12784,13604,18878,13602'},
-
-    	{query : "multifuncionais", 
-    	dictionary: 'merchantcategory=' + 'Informática' + '&' +
-					'merchantcategory=' + 'Acessórios e Periféricos' + '&' +
-					'merchantcategory=' + 'Multifuncional' + '&' +
-					'merchantcategory=' + 'Informatica / Impressoras / Impressora' + '&' +
-					'merchantcategory=' + 'Informatica / Impressoras / Multifuncional' + '&' +
-					'merchantcategory=' + 'Informática / Impressão / Impressoras',
-    	departament:"informática",
-    	programs:'12011,13212,16588,12781,12785,12784,13604,18878,13602'},
-
-    	{query : "navegador GPS", 
-    	dictionary: 'merchantcategory=' + 'Automotivo / GPS e Acessórios / GPS' + '&' +
-					'merchantcategory=' + 'Automotivo / GPS / GPS' + '&' +
-					'merchantcategory=' + 'Esporte-e-Lazer / GPS Esportivo / GPS' + '&' +
-					'merchantcategory=' + 'Informática' + '&' +
-					'merchantcategory=' + 'GPS',
-    	departament:"informática",
-    	programs:'12011,13212,16588,12781,12785,12784,13604,18878,13602'},
+	    	dictionary: 'merchantcategory=' + 'Informática / Notebooks e Conversíveis / Notebooks' + '&' +
+						'merchantcategory=' + 'Notebooks & Ultrabooks' + '&' +
+						'merchantcategory=' + 'Informatica / Notebooks / Intel Celeron' + '&' +
+						'merchantcategory=' + 'Informatica / Notebooks / Intel Core i3' + '&' +
+						'merchantcategory=' + 'Informatica / Notebooks / Intel Core i5' + '&' +
+						'merchantcategory=' + 'Informatica / Notebooks / Intel Core i7' + '&' +
+						'merchantcategory=' + 'Notebook' + '&' +
+						'merchantcategory=' + 'Nb / - / Core / I7' + '&' +
+						'merchantcategory=' + 'Nb / - / Core / I3' + '&' +
+						'merchantcategory=' + 'Nb / - / Core / I5' + '&' +
+						'merchantcategory=' + 'Nb / - / Core / I7' + '&' +
+						'merchantcategory=' + '2 / Em / 1 / - / Core / I3' + '&' +
+						'merchantcategory=' + '2 / Em / 1 / - / Core / I5' + '&' +
+						'merchantcategory=' + '2 / Em / 1 / - / Core / I7' + '&' +
+						'merchantcategory=' + 'pad / Mini / Wi-fi / + / 3g / & / 4g',
+	    	departament:"informática",
+	    	programs:'12011,13212,16588,12781,12785,12784,13604,18878,13602'},
+    	{	
+    		query : "macbook apple", 
+	    	dictionary: 'merchantcategory=' + 'Informática / Notebooks e Conversíveis / Notebooks' + '&' +
+						'merchantcategory=' + 'Mochilas para Notebook' + '&' +
+						'merchantcategory=' + 'Notebook',
+	    	departament:"informática",
+	    	programs:'12011,13212,16588,12781,12785,12784,13604,18878,13602'
+	    },
+    	{
+    		query : "impressoras", 
+	    	dictionary: 'merchantcategory=' + 'Jato de Tinta' + '&' +
+						'merchantcategory=' + 'Laser Monocromática' + '&' +
+						'merchantcategory=' + 'Laser Colorida' + '&' +
+						'merchantcategory=' + 'Laser P&B' + '&' +
+						'merchantcategory=' + 'Informatica / Impressoras / Multifuncional' + '&' +
+						'merchantcategory=' + 'Informática' + '&' +
+						'merchantcategory=' + 'Informática / Impressão / Impressoras' + '&' +
+						'merchantcategory=' + 'Informatica / Impressoras / Impressora',
+	    	departament:"informática",
+	    	programs:'12011,13212,16588,12781,12785,12784,13604,18878,13602'
+	    },
+    	// {
+    	// 	query : "multifuncionais", 
+	    // 	dictionary: 'merchantcategory=' + 'Informática' + '&' +
+					// 	'merchantcategory=' + 'Acessórios e Periféricos' + '&' +
+					// 	'merchantcategory=' + 'Multifuncional' + '&' +
+					// 	// 'merchantcategory=' + 'Informatica / Impressoras / Impressora' + '&' +
+					// 	'merchantcategory=' + 'Informatica / Impressoras / Multifuncional' + '&' +
+					// 	// 'merchantcategory=' + 'Informática / Impressão / Impressoras',
+	    // 	departament:"informática",
+	    // 	programs:'12011,13212,16588,12781,12785,12784,13604,18878,13602'
+	    // },
+    	{
+    		query : "navegador GPS", 
+	    	dictionary: 'merchantcategory=' + 'Automotivo / GPS e Acessórios / GPS' + '&' +
+						'merchantcategory=' + 'Automotivo / GPS / GPS' + '&' +
+						'merchantcategory=' + 'Esporte-e-Lazer / GPS Esportivo / GPS' + '&' +
+						'merchantcategory=' + 'Informática' + '&' +
+						'merchantcategory=' + 'GPS',
+    		departament:"informática",
+    		programs:'12011,13212,16588,12781,12785,12784,13604,18878,13602'
+    	},
 
     ]
 
